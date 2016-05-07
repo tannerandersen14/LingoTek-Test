b3b7c450de1e46f8839ef9b01fe1418cf505ca8f
Tanner Andersen <tanner.andersen14@gmail.com>
2016-05-06 21:25:36 -0600

    Winston now logs http requests, even though there are none.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 LingoTek Test
 
-Run gulp in terminal to start app on port 5050.
+First use npm install and bower install to get dependencies, then run gulp in terminal to start app on port 5050.
--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var winston = require('winston');
 var expressWinston = require('express-winston');
 var connect = require('connect');
 var app = express();
+app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console({
+          json: true,
+          colorize: true
+        })
+      ],
+      meta: true,
+      msg: "HTTP {{req.method}} {{req.url}}",
+      expressFormat: true,
+      colorStatus: true,
+      ignoreRoute: function (req, res) { return false; }
+    }));
 app.use(express.static(__dirname + '/public'));
 app.listen(5050, function() {
   console.log('Listening in on port 5050');
